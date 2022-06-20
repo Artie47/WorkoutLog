f59f8fc15a563badbdbf107bea0440df117e3d61
mandr <mandrickina888@mail.ru>
2022-06-20 19:46:22 +0300

    20/06

---
+++ app/src/main/java/app/sport/workoutlog/Trener.kt
@@ -0,0 +1,11 @@
+package app.sport.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Trener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trener)
+    }
+}
